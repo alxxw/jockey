87b36f8303ee7ef60bd8d4a49ef8e396b51dfbb2
alxxwu <alxxwu@gmail.com>
2025-05-11 23:07:59 +1000

    added various features

--- monte.py
+++ monte.py
@@ -5,42 +5,29 @@ st.write("This is a Monte Carlo simulation for betting odds. It calculates the e
 
 with st.form("form"):
     st.write("Enter the betting odds for back and lay bets:")
-    bet365 = st.number_input("Bet365 Odds", value=1.65)
-    required = st.number_input("Required Wins", value=2)
+    bet365 = st.number_input("Bet365 Odds", value=2.00, format="%.2f")
+    back_plus = st.number_input("back + x%", value=0)
+    required = st.number_input("Required Wins", value=1)
     trials = st.number_input("Trials", value=1000000)
     back_odds = {}
     lay_odds = {}
     col1, col2 = st.columns(2)
-    with col1:
-        st.write("Back Odds")
-        for i in range(1, 12):
-            back_odds[i] = st.number_input(f"Race {i}", value=0, key=f"back_{i}")
-    with col2:
-        st.write("Lay Odds")
-        for i in range(1, 12):
-            lay_odds[i] = st.number_input(f"Race {i}", value=0, key=f"lay_{i}")
+    for i in range(1, 13):
+        col1, col2 = st.columns(2)
+        with col1:
+            if i == 1:
+                st.write("Back Odds")
+            back_odds[i] = st.number_input(f"Race {i}", value=0.00, key=f"back_{i}", format="%.2f")
+        with col2:
+            if i == 1:
+                st.write("Lay Odds")
+            lay_odds[i] = st.number_input(f"Race {i}", value=0.00, key=f"lay_{i}", format="%.2f")
+
+
 
     submitted = st.form_submit_button("Submit")
 
 x = random.Random()
-
-# bet365 = 1.65
-# required = 2
-# back_odds[1] = 6.8
-# back_odds[2] = 3.05
-# back_odds[3] = 2.86
-# back_odds[4] = 2.42
-# back_odds[5] = 8.4
-# back_odds[6] = 3.41
-# back_odds[7] = 2.18
-
-# lay_odds[1] = 8.6
-# lay_odds[2] = 3.59
-# lay_odds[3] = 3.45
-# lay_odds[4] = 3.7
-# lay_odds[5] = 23
-# lay_odds[6] = 8.4
-# lay_odds[7] = 2.57
 
 def calculate(setting):
     money = 0
@@ -54,25 +41,47 @@ def calculate(setting):
             prob[i] = 1 / ((back_odds[i] + lay_odds[i]) / 2)
         elif setting == "Lay Odds":
             prob[i] = 1 / lay_odds[i]
+        elif setting == "back_plus":
+            prob[i] = 1 / (back_odds[i] + (back_plus / 100 * back_odds[i]))
 
     payouts = 0
+    if len(prob) < required:
+        st.write("Please try harder. You need at least ", str(required), " races for the jockey boost to be possible.")
+        return
     for i in range(trials):
         wins = 0
 
-        for j in range(1, len(prob) + 1):
-            if not prob[j]:
-                continue
-            if x.random() < prob[j]:
+        for j in prob.values():
+            if x.random() < j:
                 wins += 1
         if wins >= required:
             payouts += 1
             money += (bet365 - 1)
         else:
             money -= 1
-    st.write(setting)
-    st.write("Odds", str(round(trials / payouts, 4)))
+    explanation = ""
+    if setting == "Probability Midpoint":
+        explanation = "This is the average of the back and lay probabilities. More aggressive than odds midpoint."
+    elif setting == "Odds Midpoint":
+        explanation = "This is the average of the back and lay odds. More conservative than probability midpoint."
+    elif setting == "Lay Odds":
+        explanation = "This is the lay odds. This is the most conservative strategy."
+    st.markdown(f"**{setting}**", help=explanation)
+    true = round(trials / payouts, 4)
+    p = 1 / true
+    q = 1 - p
+    b = bet365 - 1
+    st.write("Odds", str(true))
     st.write("ev" , str(round(money / trials * 100, 2)), "%")
+    st.write("Kelly", str(round((p * b - q) / b * 100, 2)), "%")
 if submitted:
-    calculate("Probability Midpoint")
-    calculate("Odds Midpoint")
-    calculate("Lay Odds")
+    if back_plus != 0:
+        calculate("back_plus")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        calculate("Probability Midpoint")
+    with col2:
+        calculate("Odds Midpoint")
+    with col3:
+        calculate("Lay Odds")
+st.info("Made by @shiina4904. Please feel free to message about any bugs/quirks/edge cases/suggestions to make it better! multiple jockeys without using combined odds calculator is coming soon tm but please use combined odds calculator for now.")
